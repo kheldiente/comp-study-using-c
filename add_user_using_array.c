c7c6f42665a3628d8493751c291cbdeb0bbbe4cd
Michael Diente <michael.diente@englishcentral.com>
2020-03-09 01:34:34 +0800

    Added improvement when deleting and updating user

--- add_user_using_array.c
+++ add_user_using_array.c
@@ -46,11 +46,9 @@ int sorted_char_binary_search(char *arr, int length, int size, char *element) {
     while (start <= end) {
         mid = (start + end) / 2;
         memcpy(tempValue, arr + (mid * length), length);
-        printf("\nsorted_char_binary: %s to %s", tempValue, element);
 
         int result = strcmp(tempValue, element);
         if (result == 0) {
-            printf("\nMatched element %s", element);
             return mid;
         } else if (result > 0) {
             end = mid - 1;
@@ -58,8 +56,48 @@ int sorted_char_binary_search(char *arr, int length, int size, char *element) {
             start = mid + 1;
         }
     }
-    printf("\nHas no matched :(");
     return -1;
+}
+
+int bubble_sort_and_bsearch_char(char *arr, int length, int n, char *keyword) {
+    char *tempArr = malloc(n * sizeof(char[length]));
+    int tempIndexArr[n];
+    char tempString[length]; 
+    char prevString[length];
+    char currString[length];
+    int tempIndex = -1;
+
+    int j = 0;
+    memcpy(tempArr, arr, n * length);
+    for (int x=0;x<n;x++) {
+        tempIndexArr[x] = x + 1;
+    }
+    while(j != (n - 1)) {
+        for (int i=j+1;i<n;i++) { 
+            strcpy(prevString, tempArr + (j * length));
+            strcpy(currString, tempArr + (i * length));
+            if (strcmp(prevString, currString) > 0) {
+                // Swap char array
+                strcpy(tempString, prevString); 
+                strcpy(tempArr + (j * length), currString); 
+                strcpy(tempArr + (i * length), tempString);
+
+                // Swap indices
+                tempIndex = j;
+                tempIndexArr[j] = i;
+                tempIndexArr[i] = tempIndex; 
+            }
+        }
+        j++;
+    }
+    int foundIndex = sorted_char_binary_search(tempArr, length, n, keyword);
+    if (foundIndex != -1) {
+        int origIndex = tempIndexArr[foundIndex];
+        free(tempArr);
+        return origIndex;    
+    }
+    free(tempArr);
+    return -1;   
 }
 
 char* bubble_sort_char(char *arr, int length, int n) {
@@ -94,7 +132,7 @@ char* bubble_sort_char(char *arr, int length, int n) {
     return tempArr;
 }
 
-void bubble_sort_users() { 
+void sort_users() { 
     char prevFullName[LNAME_LENGTH + FNAME_LENGTH];
     char fullName[LNAME_LENGTH + FNAME_LENGTH];
     char tempFname[FNAME_LENGTH];
@@ -107,48 +145,49 @@ void bubble_sort_users() {
 
     for (int j=0; j<n-1; j++) { 
         for (int i=j+1; i<n; i++) { 
-            strcpy(prevFullName, lastName[j]);
-            strcat(prevFullName, firstName[j]);
-            strcpy(fullName, lastName[i]);
-            strcat(fullName, firstName[i]);
+            if (userId[j] != -1) {
+                strcpy(prevFullName, lastName[j]);
+                strcat(prevFullName, firstName[j]);
+                strcpy(fullName, lastName[i]);
+                strcat(fullName, firstName[i]);
 
-            if (strcmp(prevFullName, fullName) > 0) {
-                // Swap userId
-                tempUserId = userId[j];
-                userId[j] = userId[i];
-                userId[i] = tempUserId;
+                if (strcmp(prevFullName, fullName) > 0) {
+                    // Swap userId
+                    tempUserId = userId[j];
+                    userId[j] = userId[i];
+                    userId[i] = tempUserId;
 
-                // Swap firstName
-                strcpy(tempFname, firstName[j]); 
-                strcpy(firstName[j], firstName[i]); 
-                strcpy(firstName[i], tempFname); 
+                    // Swap firstName
+                    strcpy(tempFname, firstName[j]); 
+                    strcpy(firstName[j], firstName[i]); 
+                    strcpy(firstName[i], tempFname); 
 
-                // Swap lastName
-                strcpy(tempLname, lastName[j]);
-                strcpy(lastName[j], lastName[i]);
-                strcpy(lastName[i], tempLname);
+                    // Swap lastName
+                    strcpy(tempLname, lastName[j]);
+                    strcpy(lastName[j], lastName[i]);
+                    strcpy(lastName[i], tempLname);
 
-                // Swap birthday
-                strcpy(tempBday, birthday[j]);
-                strcpy(birthday[j], birthday[i]);
-                strcpy(birthday[i], tempBday);
+                    // Swap birthday
+                    strcpy(tempBday, birthday[j]);
+                    strcpy(birthday[j], birthday[i]);
+                    strcpy(birthday[i], tempBday);
 
-                // Swap gender
-                strcpy(tempGender, gender[j]);
-                strcpy(gender[j], gender[i]);
-                strcpy(gender[i], tempGender);
+                    // Swap gender
+                    strcpy(tempGender, gender[j]);
+                    strcpy(gender[j], gender[i]);
+                    strcpy(gender[i], tempGender);
 
-                // Swap favColor
-                strcpy(tempFavColor, favColor[j]);
-                strcpy(favColor[j], favColor[i]);
-                strcpy(favColor[i], tempFavColor);
+                    // Swap favColor
+                    strcpy(tempFavColor, favColor[j]);
+                    strcpy(favColor[j], favColor[i]);
+                    strcpy(favColor[i], tempFavColor);
 
-                // Swap age
-                tempAge = age[j];
-                age[j] = age[i];
-                age[i] = tempAge;
+                    // Swap age
+                    tempAge = age[j];
+                    age[j] = age[i];
+                    age[i] = tempAge;
+                }
             }
-
         } 
     } 
 }
@@ -159,17 +198,78 @@ void display_users() {
     printf("\n----------------------------------------\n");
     
     for(int x=0;x<n;x++) {
-        printf("\n-------------------------");
-        printf("\nUser id: %d", userId[x]);
-        printf("\nFirst name: %s", firstName[x]);
-        printf("\nLast name: %s", lastName[x]);
-        printf("\nBirthday: %s", birthday[x]);
-        printf("\nAge: %d", age[x]);
-        printf("\nGender: %s", gender[x]);
-        printf("\nFavorite color: %s", favColor[x]);
-        printf("\n-------------------------\n\n");
+        if (userId[x] != -1) {
+            printf("\n-------------------------");
+            printf("\nUser id: %d", userId[x]);
+            printf("\nFirst name: %s", firstName[x]);
+            printf("\nLast name: %s", lastName[x]);
+            printf("\nBirthday: %s", birthday[x]);
+            printf("\nAge: %d", age[x]);
+            printf("\nGender: %s", gender[x]);
+            printf("\nFavorite color: %s", favColor[x]);
+            printf("\n-------------------------\n\n");
+        }
+    }
+}
+
+void display_user(int index) {
+    printf("\n\n----------------------------------------\n");
+    printf("               Result                       ");
+    printf("\n----------------------------------------\n");
+
+    printf("\n-------------------------");
+    printf("\nUser id: %d", userId[index]);
+    printf("\nFirst name: %s", firstName[index]);
+    printf("\nLast name: %s", lastName[index]);
+    printf("\nBirthday: %s", birthday[index]);
+    printf("\nAge: %d", age[index]);
+    printf("\nGender: %s", gender[index]);
+    printf("\nFavorite color: %s", favColor[index]);
+    printf("\n-------------------------\n\n");
+}
+
+int find_user(char *keyword) {
+    int foundIndex = -1;
+
+    if (foundIndex == -1) {
+        foundIndex = bubble_sort_and_bsearch_char(firstName, FNAME_LENGTH, n, keyword);
+    }
+
+    if (foundIndex == -1) {
+        foundIndex = bubble_sort_and_bsearch_char(lastName, LNAME_LENGTH, n, keyword);
+    }
+
+    if (foundIndex == -1) {
+        foundIndex = bubble_sort_and_bsearch_char(birthday, BDAY_LENGTH, n, keyword);
+    }
+
+    if (foundIndex == -1) {
+        foundIndex = bubble_sort_and_bsearch_char(gender, GENDER_LENGTH, n, keyword);
+    }
+
+    if (foundIndex == -1) {
+        foundIndex = bubble_sort_and_bsearch_char(favColor, FAVCOLOR_LENGTH, n, keyword);
     }
     
+    if (foundIndex != -1) {
+        display_user(foundIndex);
+    } else {
+        printf("\nHas no match for keyword: %s", keyword);
+    }
+    return foundIndex;
+}
+
+void search_user() {
+    char keyword[30];
+
+    while (strcmp(keyword, "exit") != 0) {
+        printf("\nEnter keyword, or else type exit: ");
+        scanf("%s", keyword);
+        
+        printf("\n\nSearching for keyword(s): %s", keyword);
+        find_user(keyword);
+        printf("\n");
+    }
 }
 
 void add_user() {
@@ -210,19 +310,72 @@ void add_user() {
 }
 
 void delete_user() {
-    char *attribute;
-    char keyword[20];
+    char keyword[30];
+    char yes[2] = "n";
 
     while (strcmp(keyword, "exit") != 0) {
         printf("\nEnter keyword, or else type exit: ");
         scanf("%s", keyword);
         
         printf("\n\nSearching for keyword(s): %s", keyword);
+        int userIndex = find_user(keyword);
+        
+        if (userIndex != -1) {
+            printf("\nDelete user %d? Y/N ", userId[userIndex]);
+            scanf("%s", yes);
 
-        char *sortedFavColor = bubble_sort_char(favColor, FAVCOLOR_LENGTH, n);
-        int foundIndex = sorted_char_binary_search(sortedFavColor, FAVCOLOR_LENGTH, n, keyword);
-        // char *sortedLastName = bubble_sort_char(lastName, LNAME_LENGTH, n);
+            if (strcmp(yes, "Y") == 0 || strcmp(yes, "y" == 0)) {
+                strcpy(firstName[userIndex], "\0");
+                strcpy(lastName[userIndex], "\0");
+                strcpy(birthday[userIndex], "\0");
+                strcpy(gender[userIndex], "\0");
+                strcpy(favColor[userIndex], "\0");
+                userId[userIndex] = -1;
+                age[userIndex] = -1;
+                n--;
+            }
+        }
+        printf("\n");
+    }
+}
 
+void update_user() {
+    char keyword[30];
+    char value[30];
+    int attribute = -1;
+
+    while (strcmp(keyword, "exit") != 0) {
+        printf("\nEnter keyword to search user, or else type exit: ");
+        scanf("%s", keyword);
+        
+        if (strcmp(keyword, "exit") != 0) {
+            int userIndex = find_user(keyword);
+            if (userIndex != -1) {
+                printf("\nAttribute number");
+                printf("\n 1 - First name");
+                printf("\n 2 - Last name");
+                printf("\n 3 - Birthday (eg: 05/02/1992)");
+                printf("\n 4 - Gender (M/F)");
+                printf("\n 5 - Favorite color (eg: blue, yellow, midnight blue, etc.)");
+
+                printf("\nEnter attribute number: ");
+                scanf("%d", &attribute);
+                printf("Enter new value: ");
+                scanf("%s", value);
+
+                if (attribute == 1) {
+                    strcpy(firstName[userIndex], value);
+                } else if (attribute == 2) {
+                    strcpy(lastName[userIndex], value);
+                } else if (attribute == 3) {
+                    strcpy(birthday[userIndex], value);
+                } else if (attribute == 4) {
+                    strcpy(gender[userIndex], value);
+                } else if (attribute == 5) {
+                    strcpy(favColor[userIndex], value);
+                }
+            }
+        }
         printf("\n");
     }
 }
@@ -284,9 +437,11 @@ int main() {
         printf("\n--------------------");
         printf("\nCommands:");
         printf("\n 1 - Add user");
-        printf("\n 2 - Display users");
-        printf("\n 3 - Delete user");
-        printf("\n 4 - Sort");
+        printf("\n 2 - Delete user");
+        printf("\n 3 - Update user");
+        printf("\n 4 - Search user");
+        printf("\n 5 - Display users");
+        printf("\n 6 - Sort users");
         printf("\n 0 - Exit");
         printf("\n--------------------");
 
@@ -297,11 +452,15 @@ int main() {
         if (command == 1) {
             add_user();
         } else if (command == 2) {
-            display_users();
-        } else if (command == 3) {
             delete_user();
+        } else if (command == 3) {
+            update_user();
         } else if (command == 4) {
-            bubble_sort_users();
+            search_user();
+        } else if (command == 5) {
+            display_users();
+        } else if (command == 6) {
+            sort_users();
         } else if (command == 0) {
             free(firstName);
             free(lastName);
